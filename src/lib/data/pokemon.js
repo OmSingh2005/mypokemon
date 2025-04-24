8926e0102c54ee3e0fd5a81faccbafa1a3d1449c
Omsingh2005 <oms1ngho2005@gmail.com>
2025-04-25 01:31:33 +0530

    data added

--- src/lib/data/pokemon.js
+++ src/lib/data/pokemon.js
@@ -1,215 +1,272 @@
 export const pokemonData = [
-  {
+    // Original Starters (First Stage)
+    {
       id: 1,
       name: "Bulbasaur",
       type: "Grass/Poison",
       sprite: "/images/pokemon/1.png",
-      stats: {
-          hp: 45,
-          attack: 49,
-          defense: 49,
-          specialAttack: 65,
-          specialDefense: 65,
-          speed: 45
-      }
-  },
-  {
+      stats: { hp: 45, attack: 49, defense: 49, specialAttack: 65, specialDefense: 65, speed: 45 }
+    },
+    {
       id: 4,
       name: "Charmander",
       type: "Fire",
       sprite: "/images/pokemon/4.png",
-      stats: {
-          hp: 39,
-          attack: 52,
-          defense: 43,
-          specialAttack: 60,
-          specialDefense: 50,
-          speed: 65
-      }
-  },
-  {
+      stats: { hp: 39, attack: 52, defense: 43, specialAttack: 60, specialDefense: 50, speed: 65 }
+    },
+    {
       id: 7,
       name: "Squirtle",
       type: "Water",
       sprite: "/images/pokemon/7.png",
-      stats: {
-          hp: 44,
-          attack: 48,
-          defense: 65,
-          specialAttack: 50,
-          specialDefense: 64,
-          speed: 43
-      }
-  },
-  {
-      id: 25,
-      name: "Pikachu",
-      type: "Electric",
-      sprite: "/images/pokemon/25.png",
-      stats: {
-          hp: 35,
-          attack: 55,
-          defense: 40,
-          specialAttack: 50,
-          specialDefense: 50,
-          speed: 90
-      }
-  },
-  {
-      id: 94,
-      name: "Gengar",
-      type: "Ghost/Poison",
-      sprite: "/images/pokemon/94.png",
-      stats: {
-          hp: 60,
-          attack: 65,
-          defense: 60,
-          specialAttack: 130,
-          specialDefense: 75,
-          speed: 110
-      }
-  },
-  {
-      id: 143,
-      name: "Snorlax",
-      type: "Normal",
-      sprite: "/images/pokemon/143.png",
-      stats: {
-          hp: 160,
-          attack: 110,
-          defense: 65,
-          specialAttack: 65,
-          specialDefense: 110,
-          speed: 30
-      }
-  },
-  {
+      stats: { hp: 44, attack: 48, defense: 65, specialAttack: 50, specialDefense: 64, speed: 43 }
+    },
+  
+    // Fully Evolved Starters
+    {
       id: 6,
       name: "Charizard",
       type: "Fire/Flying",
       sprite: "/images/pokemon/6.png",
-      stats: {
-          hp: 78,
-          attack: 84,
-          defense: 78,
-          specialAttack: 109,
-          specialDefense: 85,
-          speed: 100
-      }
-  },
-  {
-      id: 9,
-      name: "Blastoise",
-      type: "Water",
-      sprite: "/images/pokemon/9.png",
-      stats: {
-          hp: 79,
-          attack: 83,
-          defense: 100,
-          specialAttack: 85,
-          specialDefense: 105,
-          speed: 78
-      }
-  },
-  {
+      stats: { hp: 78, attack: 84, defense: 78, specialAttack: 109, specialDefense: 85, speed: 100 }
+    },
+    {
       id: 3,
       name: "Venusaur",
       type: "Grass/Poison",
       sprite: "/images/pokemon/3.png",
-      stats: {
-          hp: 80,
-          attack: 82,
-          defense: 83,
-          specialAttack: 100,
-          specialDefense: 100,
-          speed: 80
-      }
-  },
-  {
-      id: 150,
-      name: "Mewtwo",
-      type: "Psychic",
-      sprite: "/images/pokemon/150.png",
-      stats: {
-          hp: 106,
-          attack: 110,
-          defense: 90,
-          specialAttack: 154,
-          specialDefense: 90,
-          speed: 130
-      }
-  },
-  {
-      id: 248,
-      name: "Tyranitar",
-      type: "Rock/Dark",
-      sprite: "/images/pokemon/248.png",
-      stats: {
-          hp: 100,
-          attack: 134,
-          defense: 110,
-          specialAttack: 95,
-          specialDefense: 100,
-          speed: 61
-      }
-  },
-  {
-      id: 149,
-      name: "Dragonite",
-      type: "Dragon/Flying",
-      sprite: "/images/pokemon/149.png",
-      stats: {
-          hp: 91,
-          attack: 134,
-          defense: 95,
-          specialAttack: 100,
-          specialDefense: 100,
-          speed: 80
-      }
-  },
-  {
-      id: 135,
-      name: "Jolteon",
+      stats: { hp: 80, attack: 82, defense: 83, specialAttack: 100, specialDefense: 100, speed: 80 }
+    },
+    {
+      id: 9,
+      name: "Blastoise",
+      type: "Water",
+      sprite: "/images/pokemon/9.png",
+      stats: { hp: 79, attack: 83, defense: 100, specialAttack: 85, specialDefense: 105, speed: 78 }
+    },
+    {
+      id: 254,
+      name: "Sceptile",
+      type: "Grass",
+      sprite: "/images/pokemon/254.png",
+      stats: { hp: 70, attack: 85, defense: 65, specialAttack: 105, specialDefense: 85, speed: 120 }
+    },
+    {
+      id: 260,
+      name: "Swampert",
+      type: "Water/Ground",
+      sprite: "/images/pokemon/260.png",
+      stats: { hp: 100, attack: 110, defense: 90, specialAttack: 85, specialDefense: 90, speed: 60 }
+    },
+    {
+      id: 392,
+      name: "Infernape",
+      type: "Fire/Fighting",
+      sprite: "/images/pokemon/392.png",
+      stats: { hp: 76, attack: 104, defense: 71, specialAttack: 104, specialDefense: 71, speed: 108 }
+    },
+    {
+      id: 658,
+      name: "Greninja",
+      type: "Water/Dark",
+      sprite: "/images/pokemon/658.png",
+      stats: { hp: 72, attack: 95, defense: 67, specialAttack: 103, specialDefense: 71, speed: 122 }
+    },
+  
+    // Fan Favorites
+    {
+      id: 25,
+      name: "Pikachu",
       type: "Electric",
-      sprite: "/images/pokemon/135.png",
-      stats: {
-          hp: 65,
-          attack: 65,
-          defense: 60,
-          specialAttack: 110,
-          specialDefense: 95,
-          speed: 130
-      }
-  },
-  {
+      sprite: "/images/pokemon/25.png",
+      stats: { hp: 35, attack: 55, defense: 40, specialAttack: 50, specialDefense: 50, speed: 90 }
+    },
+    {
+      id: 94,
+      name: "Gengar",
+      type: "Ghost/Poison",
+      sprite: "/images/pokemon/94.png",
+      stats: { hp: 60, attack: 65, defense: 60, specialAttack: 130, specialDefense: 75, speed: 110 }
+    },
+    {
+      id: 143,
+      name: "Snorlax",
+      type: "Normal",
+      sprite: "/images/pokemon/143.png",
+      stats: { hp: 160, attack: 110, defense: 65, specialAttack: 65, specialDefense: 110, speed: 30 }
+    },
+    {
+      id: 130,
+      name: "Gyarados",
+      type: "Water/Flying",
+      sprite: "/images/pokemon/130.png",
+      stats: { hp: 95, attack: 125, defense: 79, specialAttack: 60, specialDefense: 100, speed: 81 }
+    },
+    {
       id: 131,
       name: "Lapras",
       type: "Water/Ice",
       sprite: "/images/pokemon/131.png",
-      stats: {
-          hp: 130,
-          attack: 85,
-          defense: 80,
-          specialAttack: 85,
-          specialDefense: 95,
-          speed: 60
-      }
-  },
-  {
+      stats: { hp: 130, attack: 85, defense: 80, specialAttack: 85, specialDefense: 95, speed: 60 }
+    },
+    {
       id: 149,
-      name: "Gyarados",
-      type: "Water/Flying",
-      sprite: "/images/pokemon/130.png",
-      stats: {
-          hp: 95,
-          attack: 125,
-          defense: 79,
-          specialAttack: 60,
-          specialDefense: 100,
-          speed: 81
-      }
-  }
-];
+      name: "Dragonite",
+      type: "Dragon/Flying",
+      sprite: "/images/pokemon/149.png",
+      stats: { hp: 91, attack: 134, defense: 95, specialAttack: 100, specialDefense: 100, speed: 80 }
+    },
+  
+    // Strong Dragon-types
+    {
+      id: 248,
+      name: "Tyranitar",
+      type: "Rock/Dark",
+      sprite: "/images/pokemon/248.png",
+      stats: { hp: 100, attack: 134, defense: 110, specialAttack: 95, specialDefense: 100, speed: 61 }
+    },
+    {
+      id: 212,
+      name: "Scizor",
+      type: "Bug/Steel",
+      sprite: "/images/pokemon/212.png",
+      stats: { hp: 70, attack: 130, defense: 100, specialAttack: 55, specialDefense: 80, speed: 65 }
+    },
+    {
+      id: 373,
+      name: "Salamence",
+      type: "Dragon/Flying",
+      sprite: "/images/pokemon/373.png",
+      stats: { hp: 95, attack: 135, defense: 80, specialAttack: 110, specialDefense: 80, speed: 100 }
+    },
+  
+    // All 8 Eeveelutions
+    {
+      id: 134,
+      name: "Vaporeon",
+      type: "Water",
+      sprite: "/images/pokemon/134.png",
+      stats: { hp: 130, attack: 65, defense: 60, specialAttack: 110, specialDefense: 95, speed: 65 }
+    },
+    {
+      id: 135,
+      name: "Jolteon",
+      type: "Electric",
+      sprite: "/images/pokemon/135.png",
+      stats: { hp: 65, attack: 65, defense: 60, specialAttack: 110, specialDefense: 95, speed: 130 }
+    },
+    {
+      id: 136,
+      name: "Flareon",
+      type: "Fire",
+      sprite: "/images/pokemon/136.png",
+      stats: { hp: 65, attack: 130, defense: 60, specialAttack: 95, specialDefense: 110, speed: 65 }
+    },
+    {
+      id: 196,
+      name: "Espeon",
+      type: "Psychic",
+      sprite: "/images/pokemon/196.png",
+      stats: { hp: 65, attack: 65, defense: 60, specialAttack: 130, specialDefense: 95, speed: 110 }
+    },
+    {
+      id: 197,
+      name: "Umbreon",
+      type: "Dark",
+      sprite: "/images/pokemon/197.png",
+      stats: { hp: 95, attack: 65, defense: 110, specialAttack: 60, specialDefense: 130, speed: 65 }
+    },
+    {
+      id: 470,
+      name: "Leafeon",
+      type: "Grass",
+      sprite: "/images/pokemon/470.png",
+      stats: { hp: 65, attack: 110, defense: 130, specialAttack: 60, specialDefense: 65, speed: 95 }
+    },
+    {
+      id: 471,
+      name: "Glaceon",
+      type: "Ice",
+      sprite: "/images/pokemon/471.png",
+      stats: { hp: 65, attack: 60, defense: 110, specialAttack: 130, specialDefense: 95, speed: 65 }
+    },
+    {
+      id: 700,
+      name: "Sylveon",
+      type: "Fairy",
+      sprite: "/images/pokemon/700.png",
+      stats: { hp: 95, attack: 65, defense: 65, specialAttack: 110, specialDefense: 130, speed: 60 }
+    },
+  
+    // Niche Pokémon
+    {
+      id: 213,
+      name: "Shuckle",
+      type: "Bug/Rock",
+      sprite: "/images/pokemon/213.png",
+      stats: { hp: 20, attack: 10, defense: 230, specialAttack: 10, specialDefense: 230, speed: 5 }
+    },
+    {
+      id: 129,
+      name: "Magikarp",
+      type: "Water",
+      sprite: "/images/pokemon/129.png",
+      stats: { hp: 20, attack: 10, defense: 55, specialAttack: 15, specialDefense: 20, speed: 80 }
+    },
+    {
+      id: 438,
+      name: "Bonsly",
+      type: "Rock",
+      sprite: "/images/pokemon/438.png",
+      stats: { hp: 50, attack: 80, defense: 95, specialAttack: 10, specialDefense: 45, speed: 10 }
+    },
+  
+    // Powerful Psychic Types
+    {
+      id: 65,
+      name: "Alakazam",
+      type: "Psychic",
+      sprite: "/images/pokemon/65.png",
+      stats: { hp: 55, attack: 50, defense: 45, specialAttack: 135, specialDefense: 95, speed: 120 }
+    },
+    {
+      id: 68,
+      name: "Machamp",
+      type: "Fighting",
+      sprite: "/images/pokemon/68.png",
+      stats: { hp: 90, attack: 130, defense: 80, specialAttack: 65, specialDefense: 85, speed: 55 }
+    },
+    {
+      id: 282,
+      name: "Gardevoir",
+      type: "Psychic/Fairy",
+      sprite: "/images/pokemon/282.png",
+      stats: { hp: 68, attack: 65, defense: 65, specialAttack: 125, specialDefense: 115, speed: 80 }
+    },
+  
+    // Legendary/Mythical
+    {
+      id: 150,
+      name: "Mewtwo",
+      type: "Psychic",
+      sprite: "/images/pokemon/150.png",
+      stats: { hp: 106, attack: 110, defense: 90, specialAttack: 154, specialDefense: 90, speed: 130 }
+    },
+    {
+      id: 151,
+      name: "Mew",
+      type: "Psychic",
+      sprite: "/images/pokemon/151.png",
+      stats: { hp: 100, attack: 100, defense: 100, specialAttack: 100, specialDefense: 100, speed: 100 }
+    },
+    {
+      id: 493,
+      name: "Arceus",
+      type: "Normal",
+      sprite: "/images/pokemon/493.png",
+      stats: { hp: 120, attack: 120, defense: 120, specialAttack: 120, specialDefense: 120, speed: 120 }
+    }
+  ];
   
   // Function to get a random selection of Pokémon
   export function getRandomPokemon(count = 10) {
